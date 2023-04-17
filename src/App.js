36d4223e6fb6051d920a6117fc79f36136c395fb
HARSHAKUMAR REDDY BALIREDDYGARI <102666296+Harshahari1207@users.noreply.github.com>
2023-04-18 00:06:37 +0530

    About component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import {Nav} from "./components/Nav";
+import { Nav } from "./components/Nav";
+import { About } from "./components/About";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <About />
     </div>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,8 @@
+export const About = () => {
+  return (
+    <div>
+      <h1>I'm Harsha</h1>
+      <p>I am passionate about Web designing. </p>
+    </div>
+  );
+};
