fc0a68e0939372046001afa5edb84d7dcf49bf83
HARSHAKUMAR REDDY BALIREDDYGARI <102666296+Harshahari1207@users.noreply.github.com>
2023-04-18 00:43:34 +0530

    About elements.

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,18 @@
+import fb from "../img/icons8-facebook.svg";
+import linkedin from "../img/icons8-linkedin.svg";
+import github from "../img/icons8-github.svg";
 export const About = () => {
   return (
     <div className="about">
-      <h1>I'm Harsha</h1>
-      <p>I am passionate about Web designing. </p>
+      <div className="aboutContact">
+        <img src={github} alt="" />
+        <img src={linkedin} alt="" />
+        <img src={fb} alt="" />
+      </div>
+      <div className="aboutContent">
+        <h1>I'm Harsha</h1>
+        <p>I am passionate about Web designing. </p>
+      </div>
     </div>
   );
 };
